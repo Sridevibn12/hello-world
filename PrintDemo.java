3ff58888d89c4f206411fc2fd1e5c00f3674f10c
Sridevibn12 <75678770+Sridevibn12@users.noreply.github.com>
2020-12-08 20:19:49 +0530

    Program2

---
+++ PrintDemo.java
@@ -0,0 +1,14 @@
+package prog2;
+
+public class PrintDemo {
+
+	public static void main(String[] args) 
+	{
+		
+            System.out.print("Line1");
+            
+            System.out.print("Line2");
+	}
+	
+
+}
